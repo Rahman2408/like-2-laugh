654f7ef389efc8dce224c8ed08f8ba6b30b78b7e
Rahman Elsirafy <rahman2408@gmail.com>
2022-01-01 08:51:01 -0500

    adds rack-cors gem to gemfile

--- app/controllers/jokes_controller.rb
+++ app/controllers/jokes_controller.rb
@@ -1,7 +1,7 @@
 class JokesController < ApplicationController
 
   def index
-    jokes = Joke.all 
-    render json: jokes
+    jokes = Joke.all
+  render json:  JSON.pretty_generate(jokes.as_json only: [:id, :joke_id, :joke])
   end
 end
