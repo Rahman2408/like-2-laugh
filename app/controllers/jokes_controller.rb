9aea3a91b2d303bfb9a07e59da78e84f4b46de49
Rahman Elsirafy <rahman2408@gmail.com>
2022-01-01 15:21:51 -0500

    adds the show method to the jokes controller

--- app/controllers/jokes_controller.rb
+++ app/controllers/jokes_controller.rb
@@ -2,6 +2,11 @@ class JokesController < ApplicationController
 
   def index
     jokes = Joke.all
-  render json:  JSON.pretty_generate(jokes.as_json only: [:id, :joke_id, :joke])
+    render json:  JSON.pretty_generate(jokes.as_json only: [:id, :joke_id, :joke])
+  end
+
+  def show 
+    joke = Joke.find(params[:id])
+    render json: JSON.pretty_generate(joke.as_json)
   end
 end
