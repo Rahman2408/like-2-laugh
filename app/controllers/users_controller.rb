eef3fc8de0f490d3aa1798f1cddf8303983dfbc8
Rahman Elsirafy <rahman2408@gmail.com>
2022-01-01 21:01:17 -0500

    User controller has actions for creating a new user, permitting neccessary params when prompted from the frontend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user 
+    else 
+      render json: {errors: user.errors.full_messages.to_sentence}
+    end
+  end
+
+
+  private
+
+  def user_params 
+    params.permit(:username, :email, :password)
+  end
+
 end
