9cca4a6d5800b22557e299d1adb50021d68afe26
Rahman Elsirafy <rahman2408@gmail.com>
2022-01-08 16:39:58 -0500

    Fixes bugs after upgrading ruby version

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :users, only: [:create]
   resources :jokes
   resources :sessions, only: [:create]
-  # get '/myjokes', to: 'jokes#myjokes'
+
   get '/me', to: 'sessions#autoLogin'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
