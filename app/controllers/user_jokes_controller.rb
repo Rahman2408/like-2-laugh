7be19768dbfbb2dd76705d2ab3aa19c660d55ac2
Rahman Elsirafy <rahman2408@gmail.com>
2022-01-07 23:24:15 -0500

    allows request from client to add custom joke to the user jokes model

--- app/controllers/user_jokes_controller.rb
+++ app/controllers/user_jokes_controller.rb
@@ -4,9 +4,21 @@ class UserJokesController < ApplicationController
     render json:  JSON.pretty_generate(jokes.as_json)
   end
 
+  def create
+    # byebug
+    joke = logged_in_user.user_jokes.create(joke_params)
+    render json: JSON.pretty_generate(joke.as_json)
+  end
+
   def show 
     joke = UserJoke.find(params[:id])
     render json: JSON.pretty_generate(joke.as_json)
   end
 
+
+  private 
+
+  def joke_params
+    params.permit(:joke_setup, :joke_punchline)
+  end
 end
